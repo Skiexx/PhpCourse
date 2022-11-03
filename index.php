613bf0531f76983703e56a43648ae3a3f0e03862
Skiexx <codeskiexx@ya.ru>
2022-11-03 18:16:53 +0700

    Homework done

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
-// Первое задание
 <?php
-$a = 3;
-$b = 5;
-$c = $a;
-$a = $b;
-$b = $c;
-?>
-
-// Второе задание
-<?php
-$a = 3;
-$b = 5;
-$a = $a + $b;
-$b = $a - $b;
-$a = $a - $b;
-?>
+var_dump(!1); // bool  false
+var_dump(!0); // bool true
+var_dump(!true); // bool false
+var_dump(2 && 3); // bool true
+var_dump(5 && 0); // bool false
+var_dump(3 || 0); // bool true
+var_dump(5 / 1); // int 5
+var_dump(1 / 5); // float 0.2
+var_dump(5 + '5string'); // PHP Warning and int 10
+var_dump('5' == 5); // bool true
+var_dump('05' == 5); // bool true
+var_dump('05' == '5'); // bool true
