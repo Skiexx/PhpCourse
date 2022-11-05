b2f4b754433a7b5548c736d8801e851311119f8f
Skiexx <codeskiexx@ya.ru>
2022-11-05 11:58:21 +0700

    Homework done

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
-var_dump(!1); // bool  false
-var_dump(!0); // bool true
-var_dump(!true); // bool false
-var_dump(2 && 3); // bool true
-var_dump(5 && 0); // bool false
-var_dump(3 || 0); // bool true
-var_dump(5 / 1); // int 5
-var_dump(1 / 5); // float 0.2
-var_dump(5 + '5string'); // PHP Warning and int 10
-var_dump('5' == 5); // bool true
-var_dump('05' == 5); // bool true
-var_dump('05' == '5'); // bool true
+$x = 5;
+if ('string') {
+    echo 'Условие выполнилось';
+} // Не пустая строка - это true, поэтому условие выполнилось
+
+if (0) {
+    echo 'Условие выполнилось';
+} // 0 - это false, поэтому условие не выполнилось
+
+if (null) {
+    echo 'Условие выполнилось';
+} // null - это false, поэтому условие не выполнилось
+
+if (5) {
+    echo 'Условие выполнилось';
+} // Любое число, кроме 0 - это true, поэтому условие выполнилось
